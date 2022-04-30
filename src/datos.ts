a614e2987400d2c4403e43ab878a740a316b2dba
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-04-30 20:38:30 -0300

    Initial commit

---
+++ src/datos.ts
@@ -0,0 +1,9 @@
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("dato");
+let rotulo = document.getElementById("rotulo");
+
+btnEnv.addEventListener("click", () => {
+  //TU CODIGO VA AQUI, POR EJEMPLO:
+  console.log("el dato ingresado es", dato.value);
+  //
+});
